716d28756d98922c0798e577a9eb9b596b67f271
Niccolo <granieriniccolo@gmail.com>
2017-11-28 17:00:48 +0000

    Fix Graphics Toggle & minor changes
    Fixed the refreshing of the graphics toggle and deleted some commented code.

--- LeapToOSC/Assets/UI/backgroundChange.cs
+++ LeapToOSC/Assets/UI/backgroundChange.cs
@@ -2,28 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class backgroundChange : MonoBehaviour {
+public class backgroundChange : MonoBehaviour
+{
 
     private Camera cam;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
         cam = GetComponent<Camera>();
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (changeState.Toggle)
         {
             cam.clearFlags = CameraClearFlags.SolidColor;
-        } else
+            cam.cullingMask = -1;
+        }
+        else
         {
             cam.clearFlags = CameraClearFlags.Nothing;
+            cam.cullingMask = 0;
+
         }
-		
-	}
+    }
 }
